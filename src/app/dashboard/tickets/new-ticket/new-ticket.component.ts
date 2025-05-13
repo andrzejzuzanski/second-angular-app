61118d177fca7ea7c42e5ad851753b07d9456234
Andrzej Zuzański <andrzejzuzanski@gmail.com>
2025-05-13 16:07:32 +0200

    Added new ticket logic and styling. Fixed suppert component styling issue

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Output,
+  ViewChild,
+} from '@angular/core';
 import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css',
 })
-export class NewTicketComponent {}
+export class NewTicketComponent {
+  @ViewChild('form') private form?: ElementRef<HTMLFormElement>;
+  @Output() add = new EventEmitter<{ title: string; request: string }>();
+
+  onSubmit(title: string, request: string) {
+    this.add.emit({ title: title, request: request });
+
+    this.form?.nativeElement.reset();
+  }
+}
--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
+import { Ticket } from './ticket.model';
+import { Title } from '@angular/platform-browser';
+import { TicketComponent } from './ticket/ticket.component';
 
 @Component({
   selector: 'app-tickets',
   standalone: true,
-  imports: [NewTicketComponent],
+  imports: [NewTicketComponent, TicketComponent],
   templateUrl: './tickets.component.html',
   styleUrl: './tickets.component.css',
 })
-export class TicketsComponent {}
+export class TicketsComponent {
+  tickets: Ticket[] = [];
+
+  onAdd(ticketData: { title: string; request: string }) {
+    this.tickets.push({
+      title: ticketData.title,
+      request: ticketData.request,
+      id: Math.random().toString(),
+      status: 'open',
+    });
+  }
+}
